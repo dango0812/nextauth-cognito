5e7a8856d2d5d4b6dff891bcad62fb14595cc733
dango0812 <ehdrbsla12000@gmail.com>
2024-09-12 02:24:00 +0900

    refactor: modified tailwind config mjs -> js

--- typescript/tailwind.config.ts
+++ typescript/tailwind.config.ts
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss";
 
-const config = {
+const config: Config = {
   content: [
     "./pages/**/*.{html,js,ts,jsx,tsx,mdx}",
     "./components/**/*.{html,js,ts,jsx,tsx,mdx}",
@@ -10,8 +10,7 @@ const config = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
         primary: {
